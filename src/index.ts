2c72bbd46187683f79352184c1492ecdd942221c
fakaha <lunastra34@gmail.com>
2024-10-20 21:51:30 +0700

    undo

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import db from "./utils/database";
 import routes from "./routes/api";
 import bodyParser from "body-parser";
 import docs from "./docs/route";
-import { join } from "path";
-import { readFileSync } from "fs";
-import swaggerUi from "swagger-ui-express";
-import swaggerOutput from "./docs/swagger_output.json";
 
 const PORT = 3000;
 
@@ -15,24 +11,6 @@ async function init() {
     await db();
 
     const app = express();
-
-    app.use(
-      "/swagger-ui",
-      express.static(join(__dirname, "../../node_modules/swagger-ui-dist"))
-    );
-
-    const swaggerCss = readFileSync(
-      join(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"),
-      "utf-8"
-    );
-
-    app.use(
-      "/documentation",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerOutput, {
-        customCss: swaggerCss,
-      })
-    );
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
