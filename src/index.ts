0a022ddca5e3193882c85489afda059bfcba2e87
fakaha <lunastra34@gmail.com>
2024-10-20 21:48:48 +0700

    feat: add swagger for vercel

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import db from "./utils/database";
 import routes from "./routes/api";
 import bodyParser from "body-parser";
 import docs from "./docs/route";
+import { join } from "path";
+import { readFileSync } from "fs";
 import swaggerUi from "swagger-ui-express";
-import swaggerAutogen from "swagger-autogen";
+import swaggerOutput from "./docs/swagger_output.json";
 
 const PORT = 3000;
 
@@ -14,25 +16,30 @@ async function init() {
 
     const app = express();
 
+    app.use(
+      "/swagger-ui",
+      express.static(join(__dirname, "../../node_modules/swagger-ui-dist"))
+    );
+
+    const swaggerCss = readFileSync(
+      join(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"),
+      "utf-8"
+    );
+
+    app.use(
+      "/documentation",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerOutput, {
+        customCss: swaggerCss,
+      })
+    );
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.static("public"));
-
-    const outputFile = "./docs/swagger_output.json";
-    const endpointsFiles = ["./routes/*.js"];
-
-    // Generate the swagger output file
-    await swaggerAutogen(outputFile, endpointsFiles);
-
-    // Load the generated swagger document
-    const swaggerDocument = require(outputFile);
-
-    // Setup swagger-ui
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     app.get("/", (req, res) => {
       res.status(200).json({
-        message: "Welcome to SanberShop API",
+        message: "Welcome to SanberShop API"
       });
     });
 
