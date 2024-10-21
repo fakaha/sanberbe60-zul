362d0c0e73c9c4252fb889134c362b2b8926953f
fakaha <lunastra34@gmail.com>
2024-10-21 22:24:40 +0700

    fix: fixed url server for vercel

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -8,12 +8,12 @@ const doc = {
   },
   servers: [
     {
-      url: "http://localhost:3000/api",
-      description: "Local Server",
-    },
-    {
       url: "https://sanberbe60-zul.vercel.app/api/",
       description: "Vercel Server",
+    },
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
     },
   ],
   components: {
